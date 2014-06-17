a962a08251973af7b4e94591fca4162b9ff0a966
Juchnowski <sjuchnowski@gmail.com>
2014-06-17 15:32:44 +0200

    Update kod.cs
    No to jest dłuższe :p

--- kod.cs
+++ kod.cs
@@ -1,3 +1,24 @@
-int a = 2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine(a);
+namespace RozliczeniePracownikow
+{
+    public class Rozliczenia
+    {
+        public virtual double kosztPrzejazduSamochod(int kilometry, float spalanie, float cenaLitr, float dodatkowe) 
+        {
+            return Math.Round((kilometry / 100) * spalanie * cenaLitr + dodatkowe,2); 
+        
+        }
+
+        public decimal chorobowe(int liczbaDni, float wynagrodzenie, float stawkaChorobowa = 0.8f) 
+        { 
+            return Math.Round((decimal)( (wynagrodzenie*12/360)*liczbaDni*stawkaChorobowa ),1);
+        
+        }
+ 
+    }
+}
